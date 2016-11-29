61cd6665d960c7d92f969148bff3fc606ffc9cb8
imdan2018 <36336449@qq.com>
2016-11-29 08:18:05 +0800

    xx

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello Java~£¡");
 		System.out.println("Hello Java~£¡");
 		System.out.println("Hello Java~£¡");
+		System.out.println("Hello Java~£¡");
 	}
 
 }
